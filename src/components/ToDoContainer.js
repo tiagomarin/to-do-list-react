3d5cdfb1ddd40c19849b196796cc8bea13c5633a
Tiago Marin <tiago.lelinski@gmail.com>
2022-08-22 18:53:47 -0300

    add event handlers to: toggle checkbox,delete item and add item

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,22 +1,62 @@
 import React from "react"
 import Header from "./Header";
+import AddItemInput from "./AddItemInput";
 import ToDosListPlaceholder from "./ToDosListPlaceholder";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
+
+  checkBoxToggle = (id) => {
+    this.setState(prevState => ({
+      toDoItems: prevState.toDoItems.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        };
+        return item;
+      })
+    })
+    );
+  };
+
+  deleleItem = (id) => {
+    this.setState({
+      toDoItems: [
+        ...this.state.toDoItems.filter(item => {
+          return item.id !== id;
+        })
+      ]
+    }
+    );
+  };
+
+  addTodoItem = title => {
+    const newToDoItem = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    this.setState({
+      toDoItems: [...this.state.toDoItems, newToDoItem]
+    })
+  };
+
   state = {
     toDoItems: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false
       }
@@ -27,6 +67,7 @@ class TodoContainer extends React.Component {
     return (
       <>
         <Header />
+        <AddItemInput addTodoProps={this.addTodoItem} />
         <ToDosListPlaceholder
           toDoItems={this.state.toDoItems}
           checkBoxToggleProps={this.checkBoxToggle}
